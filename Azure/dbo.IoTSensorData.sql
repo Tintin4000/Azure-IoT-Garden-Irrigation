228b63eeaaa0e9c1880670ab7c62b939f049762b
Tintin4000 <30440427+Tintin4000@users.noreply.github.com>
2020-06-14 13:16:09 +0200

    Update dbo.IoTSensorData.sql
    Updated schema

--- Azure/dbo.IoTSensorData.sql
+++ Azure/dbo.IoTSensorData.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[IoTSensorData] (
     [Id]               BIGINT        IDENTITY (1, 1) NOT NULL,
     [sensordatetime]   DATETIME2 (7) NOT NULL,
-    [deviceid]         NCHAR (25)    NOT NULL,
-    [devicetype]       NCHAR (25)    NOT NULL DEFAULT 'N/A',
-    [location]         NCHAR (25)    NOT NULL DEFAULT 'N/A',
+    [sensorid]         NCHAR (25)    NOT NULL,
+    [devicetype]       NCHAR (25)    DEFAULT ('N/A') NOT NULL,
+    [location]         NCHAR (25)    DEFAULT ('N/A') NOT NULL,
+    [battery_volt]     FLOAT (53)    DEFAULT ((0)) NULL,
+    [status] TINYINT NULL, 
     [air_temperature]  FLOAT (53)    NULL,
     [air_humidity]     FLOAT (53)    NULL,
     [soil_temperature] FLOAT (53)    NULL,
-    [soil_humidity]    FLOAT (53)    NULL,
+    [soil_humidity]    FLOAT (53)    NULL,    
     CONSTRAINT [PK_ID] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-
